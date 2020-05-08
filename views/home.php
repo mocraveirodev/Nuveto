a3e5a27efaeb1b54ea9ed6d0d5297c9a54126e44
Monica Craveiro <craveiromonica@gmail.com>
2020-05-08 14:37:52 -0300

    Acertando metas no head

--- views/home.php
+++ views/home.php
@@ -4,10 +4,10 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <meta name="description" content="Fuse IoT é uma StartUp especializada em soluções IoT, que fornece transformação digital para empresas e pessoas que querem melhorar seus indicativos, controlar seus ativos e obter melhores resultados com tecnologias baseadas em IoT e Inteligência Artificial.">
-    <meta name="keywords" content="IoT,Internet of Things,Internet das Coisas,Inteligência artificial,Big Data,Analytics,Hardware,Conectividade,Segurança">
+    <meta name="description" content="Conheça a Nuveto, uma empresa inovadora de tecnologia para Atendimento ao cliente e Customer Experience CX. Visando a transformação das estratégias para melhorar o relacionamento das marcas com seus clientes, ajudando-as a se diferenciar neste cenário extremamente competitivo. Descubra o Cloud Solution Center.">
+    <meta name="keywords" content="Nuveto,Atendimento ao Cliente,CX,Customer Experience,Relacionamento ao cliente,Cloud,Cloudcomputing,CallCenter,Call-Center,Cloud Solution Center">
     <meta name="author" content="Monica Craveiro">
-    <title>Nuveto</title>
+    <title>Nuveto - Cloud Solution Center</title>
     <link rel="shortcut icon" href="views/img/favicon.ico" type="image/x-icon">
     <link rel="icon" href="views/img/favicon.ico" type="image/x-icon">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
